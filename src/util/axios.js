d741e99b03a30d00697b5bcda606e1c1bc92780b
amine-amroussi <amineamro500@gmail.com>
2023-04-25 07:19:53 +0000

    setup cookies

--- src/util/axios.js
+++ src/util/axios.js
@@ -2,10 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://blue-outstanding-shrimp.cyclic.app/api/v1",
-  withCredentials : true,
-  headers:{
-    'Control-Allow-Origin' : "*"
-  }
 });
 
 export default instance;
