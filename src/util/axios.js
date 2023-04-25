915271100b3b6363e9f9608d9079588fdf6ca6a9
amine-amroussi <amineamro500@gmail.com>
2023-04-25 01:07:49 +0000

    setup

--- src/util/axios.js
+++ src/util/axios.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://blue-outstanding-shrimp.cyclic.app/api/v1",
-  withCredentials : true
+  withCredentials : true,
+  headers:{
+    'Control-Allow-Origin' : "*"
+  }
 });
 
 export default instance;
