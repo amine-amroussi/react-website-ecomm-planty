5f36e875290d5806e9e2da918a41ac38b593f1f7
amine-amroussi <amineamro500@gmail.com>
2023-04-25 07:49:43 +0000

    setup

--- src/util/axios.js
+++ src/util/axios.js
@@ -4,4 +4,11 @@ const instance = axios.create({
   baseURL: "https://blue-outstanding-shrimp.cyclic.app/api/v1",
 });
 
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.log(error);
+  }
+);
+
 export default instance;
