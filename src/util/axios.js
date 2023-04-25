9a98b665f294ba25a41ac71feb29518e43170707
amine-amroussi <amineamro500@gmail.com>
2023-04-25 08:14:47 +0000

    setup

--- src/util/axios.js
+++ src/util/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://planty-api-dgcc.onrender.com/api/v1",
+  withCredentials: true
 });
 
 instance.interceptors.response.use(
